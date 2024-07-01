1ec7676bead23be3f74d80ec2adf9915fe9bd122
sadeqsoli <solisadeq@gmail.com>
2024-07-01 12:41:07 +0330

    finishing day1 test.

---
+++ MyCode/01b_band_name_generator.py
@@ -0,0 +1,17 @@
+# username = input('what is your name?\n')
+# print('there is ' + str(len(username)) + " letter in the name " + username)
+
+# a = input("a:")
+# b = input("b:")
+
+# c = a
+# a = b
+# b = c
+
+# print("a: " + a)
+# print("b: " + b)
+
+print('Wellcome to the band name generator!')
+city_name = input("what city did you grew up in?\n")
+user_name = input("what is your name?\n")
+print('your band name could be '+ city_name + " " + user_name)
